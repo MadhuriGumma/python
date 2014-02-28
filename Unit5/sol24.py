e17124804277d892aaababa71cbed94d3de99d10
Adarsh Yagnik <adarsh_yagnik@yahoo.com>
2014-02-28 18:16:31 +0530

    Recoded sol24, added graph

--- Unit5/sol24.py
+++ Unit5/sol24.py
@@ -1,32 +1,61 @@
 # Problem24: GDP and Unemployability
+import numpy as np
+import matplotlib.pyplot as pl
 
-def getgdpavg(filename, year):	
-	with open(filename) as g:
-		glist = g.read().strip().split(",")
-	s = 0.0
-	si = (year - 1948)*4
-	for i in range(si, si + 4):
-		s = s + float(glist[i])
-	return s/4.0
+glist = {}
+ulist = {}
 
-def getunempavg(filename, year):
+def readgdp(gf):
+	global glist
+	with open(gf) as g:
+		r = g.read().strip().split("\n")
+		for i in range(1948, 2010):
+			arr = [float((x.split(" "))[-1]) for x in r if x[:4] == str(i)]
+			if arr:
+				glist[i] = sum(arr)/len(arr)
+
+def readunemp(uf):
+	global ulist
 	with open(uf) as u:
-		ulist = u.read().strip().split(",")
-		si = (y - 1948)*12
-		s = 0.0
-	for i in range(si, si + 12):
-		s = s + float(ulist[i])
-	return s/12.0
+		r = u.read().strip().split("\n")[1:]
+		for i in range(0, len(r)):
+			arr = r[i].replace(",", " ").strip().split(" ")
+			vals = map(float, arr[1:])
+			y = int(arr[0])
+			if vals:
+				avg = sum(vals)/len(vals)
+				ulist[y] = avg
 
-gf = raw_input("Enter GDP filename: ")
-uf = raw_input("Enter Unemployment filename: ")
-y = int(raw_input("Year to examine: "))
-while y < 1948 or y > 2008:
-	print "Bad year, try again"
-	y = int(raw_input("Year to examine: "))
-
-g = getgdpavg(gf, y)
-u = getunempavg(uf, y)
-print "GDP:", g
-print "Unemployment:", u
+def fetchyear(y):
+	if y not in glist or y not in ulist:
+		print "Year out of range! Try again..."
+		return 0
+	else:
+		print "Average GDP in", y, ":", glist[y]
+		print "Average unemployment in", y, ":", ulist[y]
+		return 1
+def showgraph():
+	global ulist, glist
+	fig = pl.figure()
+	ax1 = fig.add_subplot(111)
+	xr = np.arange(1948, 2008, 1)
+	y1 = [(glist[i+1]-glist[i])*100/glist[i] for i in range(1948, 2008)]
+	ax1.plot(xr, y1, 'b-')
+	ax1.set_xlabel('Year')
+	ax1.set_ylabel('Change in GDP')
+	
+	ax2 = ax1.twinx()
+	y2 = [ulist[x] for x in range(1948, 2008, 1)]
+	ax2.plot(xr, y2, 'r-')
+	ax2.set_ylabel('Unemployment rate')
+	ax1.set_title('Change in GDP vs Unemployment')
+	pl.show()
+	
+if __name__ == "__main__":
+	readgdp("gdp.txt")
+	readunemp("unemp.csv")
+	while not fetchyear(int(raw_input("Enter year: "))):
+		pass
+	if raw_input("Enter Y to generate graph: ").lower() == 'y':
+		showgraph()
 
